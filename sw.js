77ff4222af40e29a7c5ad2e276dfddcc8cbfd8de
Alex <bateestah@gmail.com>
2025-08-24 12:00:45 -0400

    Remove binary icon

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'tetris-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
